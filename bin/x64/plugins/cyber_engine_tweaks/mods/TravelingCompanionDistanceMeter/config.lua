185dd4cdb6b7bf27f74478ed9b4147976a922791
Martine Dee <martine.snowfox@gmail.com>
2024-07-12 17:41:18 +0200

    Feature: TCDM Configuration
    With all the recent changes, TCDM has become a little too much
    cluttered. And perhaps too much for everyone's liking. Therefore,
    changing things a bit, so that users can decide what will be
    showed in their instance of TCDM. The present controls include:
    
        - How many data points will be considered by TCDM; this is best
        left untouched for the time being, unless you want to play
        around with it.
        - Whether the single G-force intensity will be showed
        - Whether the breakdown of the G-force vector in relation to
        player's axes will be showed.

---
+++ bin/x64/plugins/cyber_engine_tweaks/mods/TravelingCompanionDistanceMeter/config.lua
@@ -0,0 +1,10 @@
+-- Configuration
+travelingCompanionDistanceMeterConfig = {
+	-- Computation
+	dataSamplingPointsCount = 25,
+
+	-- Display
+	showGForce = true,
+		-- Won't work unless showGForce is true
+		showGComponents = false,
+};
